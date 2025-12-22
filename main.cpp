c2c7fa731ad3cbfd2f19c8ef29ecb6b7b85d78ab
Sarthak Wali <sarthakwali15@gmail.com>
2025-12-22 15:02:11 +0530

    Implemented OOP for the Cellular Automaton features and behaviour.

--- main.cpp
+++ main.cpp
@@ -2,32 +2,53 @@
 #include <windows.h>
 using namespace std;
 
-bool gridOne[] = 
-{ 
-	0, 1, 0, 1, 0, 
-	1, 0, 1, 0, 1,
-	1, 0, 0, 1, 1,
-	0, 0, 0, 1, 0,
-	1, 1, 1, 0, 1
-};
-const int n = sizeof(gridOne) / sizeof(bool);
-bool gridTwo[n];
-
-int side = 5;
-int live;
-
-void displayGen(bool* grid)
+class CA
 {
-	for (int i = 0; i < n; i++)
-	{
-		cout << grid[i];
+private:
+	int side;
+	bool* gridOne;
+	bool* gridTwo;
 
-		if ((i+1) % side == 0)
+	int live;
+	void checkCellNeighbours(int curr);
+	void nextGenCell(int curr);
+
+public:
+	CA();
+	void displayGen();
+	void processNextGen();
+	void setGridOneToNewGen();
+	~CA();
+};
+
+CA::CA()
+{
+	cout << "Enter the dimension for the n*n grid: ";
+	cin >> side;
+	
+	gridOne = (bool*)malloc(sizeof(bool) * side * side);
+	gridTwo = (bool*)malloc(sizeof(bool) * side * side);            
+
+	cout << "\nEnter the grid elements as 0 (live cell) or 1 (dead cell): ";
+	for (int i = 0; i < side * side; i++)
+	{
+		cin >> gridOne[i];
+		gridTwo[i] = gridOne[i];
+	}
+}
+
+void CA::displayGen()
+{
+	for (int i = 0; i < side * side; i++)
+	{
+		cout << gridTwo[i];
+
+		if ((i + 1) % side == 0)
 			cout << endl;
 	}
 }
 
-void checkCellNeighbours(int curr)
+void CA::checkCellNeighbours(int curr)
 {
 	live = 0;
 
@@ -38,7 +59,7 @@ void checkCellNeighbours(int curr)
 
 	bool atLeft = (curr % side) == 0;
 	bool atTop = top < 0;
-	bool atBottom = bottom >= n;
+	bool atBottom = bottom >= side * side;
 	bool atRight = ((curr + 1) % side) == 0;
 
 	if (!atLeft && gridOne[left])
@@ -59,7 +80,7 @@ void checkCellNeighbours(int curr)
 		live++;
 }
 
-void nextGenCell(int curr)
+void CA::nextGenCell(int curr)
 {
 	checkCellNeighbours(curr);
 
@@ -77,20 +98,26 @@ void nextGenCell(int curr)
 	}
 }
 
-void processNextGen()
+void CA::processNextGen()
 {
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < side * side; i++)
 	{
 		nextGenCell(i);
 	}
 }
 
-void setGridOneToNewGen()
+void CA::setGridOneToNewGen()
 {
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < side * side; i++)
 	{
 		gridOne[i] = gridTwo[i];
 	}
+}
+
+CA::~CA()
+{
+	delete[] gridOne;
+	delete[] gridTwo;
 }
 
 int main()
@@ -98,23 +125,21 @@ int main()
 	int noOfGensToSee;
 	cout << "Cellular Automaton - Conway's Game of Life" << endl << "Enter the No. of Generations to see: ";
 	cin >> noOfGensToSee;
-
+	
 	if (noOfGensToSee > 1)
 	{
+		CA ca;
 		system("cls");
-		displayGen(gridOne);
+		ca.displayGen();
 
 		while (--noOfGensToSee)
 		{
 			Sleep(1500);
 			system("cls");
 
-			processNextGen();
-			displayGen(gridTwo);
-			setGridOneToNewGen();
+			ca.processNextGen();
+			ca.displayGen();
+			ca.setGridOneToNewGen();
 		}
 	}
 }
-
-
-
